320afe94679d0aa44016c62ac7b617002cdcedb9
Pavan Kumar <jpkreddy737@gmail.com>
2026-04-13 15:45:44 +0530

    initial backend commit

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
 const UserRoute = require("./routes/userRoute");
 const ProductRoute = require("./routes/productRoute");
 const CartRoute = require("./routes/cartRoute");
@@ -11,20 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use("/users", UserRoute);
 app.use("/products", ProductRoute);
 app.use("/cart", CartRoute);
 app.use("/auth", Authroute);
 
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 60000
-})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log("Mongo Error:", err));
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5001, () => {
+  console.log("Server Started");
 });
