06d1034bd395e4bdbcba2b9baaee7b0182ca1d2a
Pavan Kumar <jpkreddy737@gmail.com>
2026-04-13 08:09:49 +0530

    fixed PORT and ready for deployment

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use("/products", ProductRoute);
 app.use("/cart", CartRoute);
 app.use("/auth", Authroute);
 
-app.listen(5001, () => {
-  console.log("Server Started");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
