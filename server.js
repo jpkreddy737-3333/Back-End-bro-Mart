90db4e5d6387c08b08a803442bd2ad892051bb8a
Pavan Kumar <jpkreddy737@gmail.com>
2026-04-13 10:50:11 +0530

    fix mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const UserRoute = require("./routes/userRoute");
 const ProductRoute = require("./routes/productRoute");
 const CartRoute = require("./routes/cartRoute");
@@ -9,10 +11,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/users", UserRoute);
 app.use("/products", ProductRoute);
 app.use("/cart", CartRoute);
 app.use("/auth", Authroute);
+
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 60000
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("Mongo Error:", err));
 
 const PORT = process.env.PORT || 5001;
 
